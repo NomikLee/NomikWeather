6720846adf8c7e43ec0e31b7718cb4cd6e039535
NomikLee <nomiklee@icloud.com>
2024-09-26 08:46:39 +0800

    Initial Commit

---
+++ WeatherTests/WeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTests.swift
+//  WeatherTests
+//
+//  Created by Pinocchio on 2024/9/26.
+//
+
+import Testing
+@testable import Weather
+
+struct WeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
